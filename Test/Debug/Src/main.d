c06b260e4e145123b9240bc788eb5b92a9266d1e
quywork62 <hoangquy08032003@gmail.com>
2025-04-05 10:36:07 +0700

    DAY 1

---
+++ Test/Debug/Src/main.d
@@ -0,0 +1,9 @@
+Src/main.o: ../Src/main.c \
+ D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h
+D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+D:/HK\ 2\ _2025/HE\ THONG\ NHUNG\ IOT/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
